298c7de861bb91da82d31138b5c662ae2c539140
Wengrzinek <LdI08j9#VQ@C>
2020-11-18 20:24:08 +0100

    Added Edit and Done buttons to the dynamic tables

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,26 +16,37 @@ tasklist.push(new task("Example3", "Test", 0, 0));
 
 
 let buildTable = () => {
-    let body = document.getElementById("tableBody")
+    let body = document.getElementById("tableBody");
     
     tasklist.forEach((t, index) => {
         let th = document.createElement("th");
-        th.scope = "row"
-        th.innerText = index
+        th.scope = "row";
+        th.innerText = index;
         
         let tdName = document.createElement("td");
-        tdName.scope = "row"
-        tdName.innerText = t.name
+        tdName.scope = "row";
+        tdName.innerText = t.name;
 
         let tdDescription = document.createElement("td");
-        tdDescription.scope = "row"
-        tdDescription.innerText = t.description
+        tdDescription.scope = "row";
+        tdDescription.innerText = t.description;
 
+        //Ought to open a dynamic edit screen
+        let tdEdit = document.createElement("input");
+        tdEdit.setAttribute("type", "button");
+        tdEdit.setAttribute("value", "Edit");
+
+        //If the Task is done, it will be removed from display and added to the "Done"-category which cannot be removed.
+        let tdCheck = document.createElement("input");
+        tdCheck.setAttribute("type", "button");
+        tdCheck.setAttribute("value", "Done");
 
         let tr = document.createElement("tr");
         tr.appendChild(th);
         tr.appendChild(tdName);
-        tr.appendChild(tdDescription)
+        tr.appendChild(tdDescription);
+        tr.appendChild(tdEdit);
+        tr.appendChild(tdCheck);
 
         body.appendChild(tr);
     })
