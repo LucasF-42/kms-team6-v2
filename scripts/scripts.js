f93ae26eaf015e675c0d2ca372ddce8485f408c3
Wengrzinek <LdI08j9#VQ@C>
2020-11-18 18:03:00 +0100

    Added task and list to scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+class task{
+    constructor(name, description, category, status) {
+        this.name= name;
+        this.description = description;
+        this.category = status;
+        this.status=status;
+    }
+}
+
+let tasklist;
+let mytask = new task("Example", "Test", 0, 0);
+tasklist.push(mytask);
